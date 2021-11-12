40b0f124892d3585dcaa100f73235495bfb60f6c
JoaoZati <joaozati@gmail.com>
2021-11-12 20:52:40 -0300

    Part 2: Home Template (create assert_contains test); - django_assertions.py: create assert_contains function; - test_home.py: create test carousel and featurette

--- base/tests/test_home.py
+++ base/tests/test_home.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from main.django_assertions import assert_contains
+
 
 @pytest.fixture
 def resp(client):
@@ -10,3 +12,12 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_carousel(resp):
+    assert_contains(resp, '<img src="/static/img/python_carousel.jpg"')
+
+
+def test_featurette(resp):
+    assert_contains(resp, '<h2 class="featurette-heading">Projeto Django Devpro.')
+
