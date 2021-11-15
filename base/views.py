81092ee3dd6abb29bbe6c4d1bb1a1b30d40da98e
JoaoZati <joaozati@gmail.com>
2021-11-15 15:06:04 -0300

    Part 3: Add models skill to home page: - test_home.py: add tests for title of skills and skills types and fix database tamplate error; - views.py: add facade context to home view; - home.html: add context to tamplate; - home_hardcoded.html: hardcoded tamplate (dalete later);

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from skills import facade
 
 
 def home(request):
-    context = {}
+    list_skill_types = facade.list_skill_and_skilltype()
+    context = {
+        'list_skill_types': list_skill_types,
+    }
     return render(request, 'base/home.html', context)
