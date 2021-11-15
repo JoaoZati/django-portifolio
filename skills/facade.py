87021416bf8de79af1b2b885f701f5ef5239b49c
JoaoZati <joaozati@gmail.com>
2021-11-15 12:21:12 -0300

    Part 3: Create list_skill_and_skilltype: - facade.py: create function that return a list with SkillTypes objects with skills atribute where skills have a list with all skills; - test_facade.py: add test for the acada, one right and other wrong;

--- skills/facade.py
+++ skills/facade.py
@@ -1,5 +1,14 @@
 from skills.models import Skill, SkillType
+from django.db.models import Prefetch
 
 
-def list_skill_type():
+def list_skilltype():
     return list(SkillType.objects.order_by('title'))
+
+
+def list_skill_and_skilltype():
+    ordered_skills = Skill.objects.order_by('title')
+    return list(SkillType.objects.order_by('title').prefetch_related(
+        Prefetch('skill_set', queryset=ordered_skills, to_attr='skills')
+    ).all())
+
--- skills/tests/test_facade.py
+++ skills/tests/test_facade.py
@@ -2,7 +2,7 @@ import pytest
 from model_mommy import mommy
 
 from skills import facade
-from skills.models import SkillType
+from skills.models import SkillType, Skill
 
 
 @pytest.fixture
@@ -10,7 +10,48 @@ def skill_types(db):
     return [mommy.make(SkillType, title=s) for s in 'title1 title3 title2'.split()]
 
 
-def test_list_skill_type(skill_types):
+@pytest.fixture
+def skill_types_ordered(db):
+    return [mommy.make(SkillType, title=s) for s in 'title1 title2 title3'.split()]
+
+
+@pytest.fixture
+def skills(skill_types):
+    list_skill = []
+    for type in skill_types:
+        list_skill.extend(mommy.make(Skill, 3, type=type))
+    return list_skill
+
+
+@pytest.fixture
+def skills_ordered(skill_types_ordered):
+    list_skill = []
+    for type in skill_types_ordered:
+        mommys = mommy.make(Skill, 3, type=type)
+        sorted_mommys = sorted(mommys, key=lambda mommy: mommy.title)
+        list_skill.extend(sorted_mommys)
+    return list_skill
+
+
+def test_list_skilltype(skill_types):
     assert_list = list(sorted(skill_types, key=lambda skill_type: skill_type.title))
-    facade_list = facade.list_skill_type()
+    facade_list = facade.list_skilltype()
     assert assert_list == facade_list
+
+
+def test_list_skill_ordered(skills_ordered):
+    list_final = facade.list_skill_and_skilltype()
+    list_assert = []
+    for query in list_final:
+        list_assert.extend([skill for skill in query.skills])
+    assert_list = list(skills_ordered)
+    assert list_assert == assert_list
+
+
+def test_list_skill_not_ordered(skills):
+    list_final = facade.list_skill_and_skilltype()
+    list_assert = []
+    for query in list_final:
+        list_assert.extend([skill for skill in query.skills])
+    assert_list = list(skills)
+    assert not (list_assert == assert_list)
