99572e02b9e0e8eb8dac46d97249b4c4848788a2
JoaoZati <joaozati@gmail.com>
2021-11-15 17:30:16 -0300

    Part_4: Fixing test_home ImageField error; - test_home.py: SimpleUploadedFile adn simple GIF; - models.py: fix flake8;

--- skills/models.py
+++ skills/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class SkillType(models.Model):
     title = models.CharField(max_length=64, unique=True)
-    static_image = models.ImageField(upload_to = 'images')
+    static_image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.title
