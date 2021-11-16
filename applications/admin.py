6c4e815ab5ada1dc8730ac13f0c2e028d545785b
JoaoZati <joaozati@gmail.com>
2021-11-16 16:00:22 -0300

    Part_4: Create Models; - models.py: create models ApplicationType and Application; - admin.py: add models to django admin;

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin # noqa
+from django.contrib import admin
+from ordered_model.admin import OrderedModelAdmin
 
-# Register your models here.
+from applications.models import Application, ApplicationType
+
+
+@admin.register(ApplicationType)
+class ApplicationTypeAdmin(OrderedModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'slug',
+        'online_app',
+        'img',
+        'move_up_down_links',
+    )
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Application)
+class ApplicationAdmin(OrderedModelAdmin):
+    list_display = (
+        'title',
+        'content',
+        'slug',
+        'link_github',
+        'link_site',
+        'application_type',
+        'move_up_down_links',
+    )
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('application_type',)
+    ordering = ('application_type', 'title')
--- applications/models.py
+++ applications/models.py
@@ -3,37 +3,37 @@ from ordered_model.models import OrderedModel # noqa
 from django.urls import reverse # noqa
 
 
-# class ApplicationType(OrderedModel):
-#     title = models.CharField(max_length=64)
-#     description = models.TextField()
-#     content = models.TextField()
-#     slug = models.SlugField(unique=True)
-#     img = models.ImageField(upload_to='images')
-#
-#     class Meta(OrderedModel.Meta):
-#         pass
-#
-#     def __str__(self):
-#         return self.title
-#
-#     def get_absolute_url(self):
-#         return reverse('applications:description', self.slug)
-#
-#
-# class Application(OrderedModel):
-#     title = models.CharField(max_length=64)
-#     content = models.TextField()
-#     slug = models.SlugField()
-#     link_github = models.TextField()
-#     link_site = models.TextField()
-#     application_type = models.ForeignKey('ApplicationType', on_delete=models.CASCADE)
-#     order_with_respect_to = 'application_type'
-#
-#     class Meta(OrderedModel):
-#         pass
-#
-#     def __str__(self):
-#         return self.title
-#
-#     def get_absolute_url(self):
-#         return reverse('applications:project')
+class ApplicationType(OrderedModel):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    online_app = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True)
+    img = models.ImageField(upload_to='images')
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('applications:description', kwargs={'slug': self.slug})
+
+
+class Application(OrderedModel):
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    slug = models.SlugField()
+    link_github = models.TextField()
+    link_site = models.TextField()
+    application_type = models.ForeignKey('ApplicationType', on_delete=models.CASCADE)
+    order_with_respect_to = 'application_type'
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('applications:project', kwargs={'slug': self.slug})
