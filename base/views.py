d9c0b788b4e07831f338e0ed537c4145675a8be3
JoaoZati <joaozati@gmail.com>
2021-11-17 08:38:34 -0300

    Part_4: Implemented carousel home; - test_application_home.py: create test applications titles; - views.py: add list_application dropdown to context; - home.html: implemented carousel dinamic template;

--- base/views.py
+++ base/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from skills import facade
+from skills import facade as sk_facade
+from applications import facade as app_facade
 
 
 def home(request):
-    list_skill_types = facade.list_skill_and_skilltype()
+    list_skill_types = sk_facade.list_skill_and_skilltype()
+    list_application_dropdown = app_facade.list_applicationtype()
     context = {
         'list_skill_types': list_skill_types,
+        'list_application_dropdown': list_application_dropdown,
     }
     return render(request, 'base/home.html', context)
